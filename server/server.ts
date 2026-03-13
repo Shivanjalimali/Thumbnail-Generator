4bd80db35f955fa9ee6591b0688920353415644d
Shivanjalimali <Shivanjali7266@gmail.com>
2026-03-13 22:18:46 +0530

    the backend url of render is added to both frontend and backends origin also

--- server/server.ts
+++ server/server.ts
@@ -24,7 +24,8 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
   "http://localhost:5175",
-  "http://localhost:5000"
+  "http://localhost:5000",
+  "https://thumbnail-generator-r94m.onrender.com"
 
 ];
 
